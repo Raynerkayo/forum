f89e63144ae89172529633ecb0a0c2292120287a
Kayo Rayner <Raynerkayo@users.noreply.github.com>
2021-10-28 22:18:16 -0300

    Update CursoRepository.java

--- src/main/java/br/com/alura/forum/repository/CursoRepository.java
+++ src/main/java/br/com/alura/forum/repository/CursoRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.alura.forum.modelo.Curso;
 
 public interface CursoRepository extends JpaRepository<Curso, Long>{
-
+	//criar tbm com um select. 
+	//@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+        //List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso")(String nomeCurso);
 	Curso findByNome(String nome);
 	
 }
